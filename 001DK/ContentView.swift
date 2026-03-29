ad4413567fc06833b39df0bbe94a554943ed3cbc
Aytacio Achilles <aytacioachilles@gmail.com>
2026-03-29 14:18:01 +0300

    Initial Commit

---
+++ 001DK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  001DK
+//
+//  Created by Aytac Akyildiz on 29/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 001DK/_01DKApp.swift
@@ -0,0 +1,17 @@
+//
+//  _01DKApp.swift
+//  001DK
+//
+//  Created by Aytac Akyildiz on 29/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct _01DKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
