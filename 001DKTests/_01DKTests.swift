cdb83698eaf5b43710a0cdd66d322f543e4b4a99
Aytacio Achilles <aytacioachilles@gmail.com>
2026-03-30 21:39:14 +0300

    Pass criteria enhanced. unit tests added

---
+++ 001DKTests/_01DKTests.swift
@@ -0,0 +1,18 @@
+//
+//  _01DKTests.swift
+//  001DKTests
+//
+//  Created by Aytac Akyildiz on 30/03/2026.
+//
+
+import Testing
+
+struct _01DKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
